7f316ee5aafde3fa00ef07f097a42517eb39c1b5
gitprajapati <77141376+gitprajapati@users.noreply.github.com>
2022-07-21 00:25:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ st.title('Multiplication of 2 numbers')
 a=st.number_input('Enter 1st number')
 b=st.number_input('Enter 2nd number')
 st.write("Multiplication of 2 numbers is = ",a*b)
+if __name__ == '__main__':
+  main()
