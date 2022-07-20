76584982aa7db1d7ea946308b79c030ebe658a2d
gitprajapati <77141376+gitprajapati@users.noreply.github.com>
2022-07-21 01:07:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"21f1003546@student.onlinedegree.iitm.ac.in\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
